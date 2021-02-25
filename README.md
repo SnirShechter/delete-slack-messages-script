5045b0e2a264638bce4d603333680b458fe02216
Snir Shechter <snir@aporia.com>
2021-02-25 12:21:50 +0200

    Adds bestEffort improvements

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ Deletes all slack messages from the #general channel.
 
 Apparently, Slack does not allow to delete the #general channel or easily clean its history ([see more info](https://slack.com/intl/en-il/help/articles/220105027-The-general-channel)). Deleting the messages one by one is annoying, hence this script.
 
+Please notice that the script does not yet handle threads properly. In the event of threads, you will need to delete the in-thread replies manually before it can continue.
 ## Instructions
 
+-   Clone the project
+-   Run `npm install`
 -   Get the workspace url and a user with admin permissions.
 -   Replace the WORKSPACE_URL, EMAIL and PASSWORD variable values at the top of the script. 
 -   Run the script and wait for the magic to happen :)
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PASSWORD = 'password'
 ;(async () => {
 	const browser = await webkit.launch({
 		headless: false,
+		slowMo: 200
 	})
 
 	// Navigate to Slack
@@ -21,22 +22,36 @@ const PASSWORD = 'password'
 
 	// Close the annoying full screen dialog
 	try {
-		await page.waitForSelector('button[aria-label="Close"]')
-		await page.click('button[aria-label="Close"]')
+		await page.waitForSelector('button[data-qa="close_fullscreen_modal"]')
+		await page.click('button[data-qa="close_fullscreen_modal"]')
 	} catch {
 		console.warn("Could not close the full screen dialog. It's possible that it just did not appear.")
 	}
 	// Delete last message
+	let tryCount = 0
 	while (true) {
+		if (tryCount >= 5) break
+		tryCount++
 		// Wait for the message element (since it might be loading it)
-		await page.waitForSelector('.c-virtual_list__item:last-child .c-message_kit__background')
+		await bestEffort(() =>
+			page.waitForSelector('.c-virtual_list__item:last-child .c-message_kit__background', {
+				timeout: 1000,
+			})
+		)
 		// Hover to show the message menu
-		await page.hover('.c-virtual_list__item:last-child .c-message_kit__background')
+		await bestEffort(() => page.hover('.c-virtual_list__item:last-child .c-message_kit__background',{timeout:1000}))
 		// Message menu: click the three dots ("More actions") to open the sub-menu
-		await page.click('.c-message__actions button[data-qa="more_message_actions"]')
+		await bestEffort(() => page.click('.c-message__actions button[data-qa="more_message_actions"]',{timeout:1000}))
 		// Click "delete message" in the sub-menu
-		await page.click('.p-message_actions_menu__delete_message')
+		await bestEffort(() => page.click('.p-message_actions_menu__delete_message',{timeout:1000}))
 		// Confirm the delete dialog
-		await page.click('button[data-qa="dialog_go"]')
+		await bestEffort(() => page.click('button[data-qa="dialog_go"]',{timeout:1000}))
+		tryCount = 0
 	}
 })()
+
+async function bestEffort(func) {
+	try {
+		return await func()
+	} catch (error) {}
+}
